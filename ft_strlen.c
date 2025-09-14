634db814b2a1a751ca8b29830fd8020882bba5c6
JulioSouza09 <juliosouza.ux@gmail.com>
2025-09-14 14:49:20 +0100

    add null check on ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: jcesar-s <jcesar-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:23:05 by jcesar-s          #+#    #+#             */
-/*   Updated: 2025/04/16 15:34:33 by jcesar-s         ###   ########.fr       */
+/*   Updated: 2025/09/14 14:48:51 by jcesar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	len;
 
+	if (!str)
+		return (0);
 	len = 0;
 	while (str[len] != '\0')
 		len++;
